9adeeb96f79f65647ba9f021286ea76c01c617b9
shreeyalligutti <92301897+shreeyalligutti@users.noreply.github.com>
2021-10-21 15:08:25 +0530

    hello

---
+++ Person.java
@@ -0,0 +1,17 @@
+class  Person
+{
+static String nameOfPerson="prashant";
+static int ageOfPerson=25;
+static boolean isAlive=true;
+static double weigntOfPerson;
+static long contactNumber=6547895412L;
+public static void main(String a[])
+{
+System.out.println(nameOfPerson);
+System.out.println(ageOfPerson);
+System.out.println(isAlive);
+System.out.println(weigntOfPerson);
+System.out.println(contactNumber);
+
+ }
+ }
