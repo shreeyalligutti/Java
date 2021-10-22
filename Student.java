43e1f2a06b75ab13ea4ae77af46baa10ee925d06
shreeyalligutti <92301897+shreeyalligutti@users.noreply.github.com>
2021-10-22 17:33:17 +0530

    Default and parametarized Constructor

---
+++ Student.java
@@ -0,0 +1,56 @@
+class Student{
+  String name;
+  int rno;
+  
+  public static void main(String args[])
+  {
+       Student S1 = new Student();
+	   System.out.println(S1.name);
+	   System.out.println(S1.rno);
+	   }
+	}
+	
+	class Student{
+	String name;
+	int rno;
+	
+	Student()
+	{
+	  name="abc";
+	  rno=13;
+	  }
+	  
+	  public static void main(String args[])
+	  {
+	      Student S1=new Student();
+		  System.out.println(S1.name);
+		  System.out.println(S2.rno);
+		  }
+		}
+		
+		class Student{
+	String name;
+	int rno;
+	
+	Student()
+	{
+	  name="abc";
+	  rno=13;
+	  }
+	  Student(String str, int n)
+	  {
+	     name=str;
+		 int=n;
+		 }
+	  
+	  public static void main(String args[])
+	  {
+	      Student S1=new Student();
+		  System.out.println(S1.name);
+		  System.out.println(S1.rno);
+		  System.out.println(S2.name);
+		  System.out.println(S2.rno);
+		  }
+		}
+		
+	  
