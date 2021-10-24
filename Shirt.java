7daece8c1f293dd5de517d2d9f68076b6386e972
shreeyalligutti <92301897+shreeyalligutti@users.noreply.github.com>
2021-10-24 12:10:35 +0530

    Add files via upload

---
+++ Shirt.java
@@ -0,0 +1,20 @@
+class Shirt
+{
+   String colour;
+   char size;
+ 
+   Shirt()
+   {
+	    colour="black";
+		size=30;
+   }
+   
+     public static void main(String args[])
+	 {
+	    Shirt S1=new Shirt();
+		System.out.println(S1.colour);
+		System.out.println(S1.size);
+		}
+	}
+   
+    
